a4b9832254c8f2db3ea964201b21d1f5f77f0695
Nxizy <luvictorlc@gmail.com>
2025-08-19 15:48:34 -0300

    feat: adicionando classes Instituicao Financeira, Movimentacao e o Enum TipoTransacao, foi adicionado alguns testes das classes na main

---
+++ src/InstituicaoFinanceira.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstituicaoFinanceira {
+    private int id;
+    private String nome;
+    private int idadeMinimaPermitida;
+    private Double tetoDeTransferencia;
+    private List<Conta> contas = new ArrayList<>();
+
+    public void mostrarContas(){
+        System.out.println("Lista de contas registradas em " + nome + "\n-----------------------");
+        contas.forEach(System.out::println); // Utilizei lambda pois fazer o For seria muito trabalhoso...
+    }
+
+    public void cadastrarConta(Conta conta){
+        contas.add(conta);
+    }
+
+    public void verificarIdade(int idade) {
+        if(idade <= idadeMinimaPermitida) {
+            System.out.println("Você não tem a idade mínima de "+ idadeMinimaPermitida +" para ter conta nesta instituição");
+        } else {
+            System.out.println("Você tem a idade permitida para fazer o cadastro nesta instituição.");
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdadeMinimaPermitida() {
+        return idadeMinimaPermitida;
+    }
+
+    public void setIdadeMinimaPermitida(int idadeMinimaPermitida) {
+        this.idadeMinimaPermitida = idadeMinimaPermitida;
+    }
+
+    public Double getTetoDeTransferencia() {
+        return tetoDeTransferencia;
+    }
+
+    public void setTetoDeTransferencia(Double tetoDeTransferencia) {
+        this.tetoDeTransferencia = tetoDeTransferencia;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+
+        Conta conta1 = new Conta();
+        conta1.setId(1);
+        conta1.setTipo(TipoConta.PF);
+        conta1.depositar(700);
+
+        Conta conta2 = new Conta();
+        conta2.setId(2);
+        conta2.setTipo(TipoConta.PJ);
+        conta2.depositar(1500);
+
+        InstituicaoFinanceira inst1 = new InstituicaoFinanceira();
+        inst1.setId(1);
+        inst1.setNome("Banco Brasileiro");
+        inst1.setIdadeMinimaPermitida(15);
+        inst1.setTetoDeTransferencia(50000.0);
+        inst1.cadastrarConta(conta1);
+        inst1.cadastrarConta(conta2);
+        inst1.mostrarContas();
+
+        System.out.println("-----------------");
+
+        Movimentacao mov1 = new Movimentacao(1,700.0, TipoTransacao.DEPOSITO, conta1.getId(), "Adiantamento salarial");
+
+        mov1.extrato();
+
+
     }
 }
